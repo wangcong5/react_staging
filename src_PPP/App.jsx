5763c5df7865f840d29dc1ddeab5c4e5f8bb7954
王聪 <wangcong21@meituan.com>
2021-05-23 00:19:24 +0800

    删除antd学习中的无效引用包

---
+++ src_PPP/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    // 状态在哪里，操作状态的方法就在哪里
+
+    render() {
+        return (
+            <div>
+                APP..
+            </div>
+        )
+    }
+}
